1141abfcc04425cbc8a417772e9c4635c14833fc
ronriver29 <100809645+ronriver29@users.noreply.github.com>
2025-07-22 09:43:20 +0800

    Initialize file structuure

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InvoicesPage = () => {
+  return (
+    <div>Invoices Page</div>
+  )
+}
+
+export default InvoicesPage
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,12 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Inter, Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+ 
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +14,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  );
-}
+  );}
